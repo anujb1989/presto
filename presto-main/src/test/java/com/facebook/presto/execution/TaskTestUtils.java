5da17a0e30613ca475d83f664e7c4920518cebc6
Piotr Nowojski <piotr.nowojski@teradata.com>
2016-09-08 09:46:45 +0200

    Pass spilling parameters to HashBuildOperator & LookupJoinOperator

--- presto-main/src/test/java/com/facebook/presto/execution/TaskTestUtils.java
+++ presto-main/src/test/java/com/facebook/presto/execution/TaskTestUtils.java
@@ -36,7 +36,10 @@ import com.facebook.presto.spi.connector.ConnectorTransactionHandle;
 import com.facebook.presto.spi.predicate.TupleDomain;
 import com.facebook.presto.spi.type.TestingTypeManager;
 import com.facebook.presto.spiller.FileSingleStreamSpillerFactory;
+import com.facebook.presto.spiller.GenericPartitioningSpillerFactory;
 import com.facebook.presto.spiller.GenericSpillerFactory;
+import com.facebook.presto.spiller.PartitioningSpillerFactory;
+import com.facebook.presto.spiller.SingleStreamSpillerFactory;
 import com.facebook.presto.spiller.SpillerStats;
 import com.facebook.presto.split.PageSinkManager;
 import com.facebook.presto.split.PageSourceManager;
@@ -123,6 +126,8 @@ public final class TaskTestUtils
                 new NodeTaskMap(finalizerService));
         NodePartitioningManager nodePartitioningManager = new NodePartitioningManager(nodeScheduler);
 
+        SingleStreamSpillerFactory singleStreamSpillerFactory = new FileSingleStreamSpillerFactory(new BlockEncodingManager(metadata.getTypeManager()), new SpillerStats(), new FeaturesConfig());
+        PartitioningSpillerFactory partitioningSpillerFactory = new GenericPartitioningSpillerFactory(singleStreamSpillerFactory);
         return new LocalExecutionPlanner(
                 metadata,
                 new SqlParser(),
@@ -139,6 +144,8 @@ public final class TaskTestUtils
                 new CompilerConfig(),
                 new TaskManagerConfig(),
                 new GenericSpillerFactory(new FileSingleStreamSpillerFactory(new BlockEncodingManager(metadata.getTypeManager()), new SpillerStats(), new FeaturesConfig())),
+                singleStreamSpillerFactory,
+                partitioningSpillerFactory,
                 new TestingBlockEncodingSerde(new TestingTypeManager()),
                 new PagesIndex.TestingFactory(),
                 new JoinCompiler(),
