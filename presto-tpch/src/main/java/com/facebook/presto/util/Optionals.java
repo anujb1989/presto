8303f80164e0ae1c5cc60ef887b489367c4dbc6e
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2017-08-04 11:25:28 +0200

    fixup! Add Optionals.{checkPresent,withBoth,combine} util methods
    Move Optionals class

--- presto-tpch/src/main/java/com/facebook/presto/util/Optionals.java
+++ presto-tpch/src/main/java/com/facebook/presto/util/Optionals.java
@@ -11,7 +11,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.facebook.presto.tpch.util;
+
+package com.facebook.presto.util;
 
 import java.util.Optional;
 import java.util.function.BiFunction;
